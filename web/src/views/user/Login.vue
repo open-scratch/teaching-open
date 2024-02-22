8642bc4d6c7da473526ad32e02ab8906c7220ca5
chengyu <chengyu2333@gmail.com>
2024-02-22 19:03:29 +0800

    关闭注册时，隐藏注册按钮

--- web/src/views/user/Login.vue
+++ web/src/views/user/Login.vue
@@ -89,7 +89,7 @@
         <router-link :to="{ name: 'alteration'}" class="forge-password" style="float: right;">
           忘记密码
         </router-link>
-       <router-link :to="{ name: 'register'}" class="forge-password" style="float: right;margin-right: 10px" >
+       <router-link v-if="$store.getters.sysConfig.allowReg=='1'" :to="{ name: 'register'}" class="forge-password" style="float: right;margin-right: 10px" >
           注册账户
         </router-link>
       </a-form-item>
