06c2c858894e9e547d2325ba67b135ad85143201
chengyu <chengyu2333@gmail.com>
2024-06-24 14:04:37 +0800

    本地文件支持视频进度拖动
    (cherry picked from commit 28e78fd08c781ac5bdc85775b408a682d07b85ea)

--- api/jeecg-boot-base-common/src/main/java/org/jeecg/common/system/controller/CommonController.java
+++ api/jeecg-boot-base-common/src/main/java/org/jeecg/common/system/controller/CommonController.java
@@ -213,15 +213,91 @@ public class CommonController {
 				response.setStatus(404);
 				throw new RuntimeException("文件不存在..");
 			}
-			response.setContentType("application/force-download");// 设置强制下载不打开
+//			response.setContentType("application/force-download");// 设置强制下载不打开
+			response.setContentType("application/octet-stream");
 			response.addHeader("Content-Disposition", "attachment;fileName=" + new String(file.getName().getBytes("UTF-8"),"iso-8859-1"));
-			inputStream = new BufferedInputStream(new FileInputStream(filePath));
-			outputStream = response.getOutputStream();
-			byte[] buf = new byte[1024];
-			int len;
-			while ((len = inputStream.read(buf)) > 0) {
-				outputStream.write(buf, 0, len);
+
+			response.setHeader("Accept-Ranges", "bytes");
+			// client requests a file block download start byte
+			BufferedInputStream bis = null;
+			InputStream ins = new FileInputStream(file);
+			bis = new BufferedInputStream(ins);
+
+			long p = 0L;
+			long toLength = 0L;
+			long contentLength = 0L;
+			int rangeSwitch = 0; // 0,从头开始的全文下载；1,从某字节开始的下载（bytes=27000-）；2,从某字节开始到某字节结束的下载（bytes=27000-39000）
+			long fileLength;
+			fileLength = file.length();
+			String rangBytes = "";
+			String range = request.getHeader("Range");
+			if (range != null && range.trim().length() > 0 && !"null".equals(range)) {
+				response.setStatus(javax.servlet.http.HttpServletResponse.SC_PARTIAL_CONTENT);
+				rangBytes = range.replaceAll("bytes=", "");
+				if (rangBytes.endsWith("-")) { // bytes=270000-
+					rangeSwitch = 1;
+					p = Long.parseLong(rangBytes.substring(0, rangBytes.indexOf("-")));
+					contentLength = fileLength - p; // 客户端请求的是270000之后的字节（包括bytes下标索引为270000的字节）
+				} else { // bytes=270000-320000
+					rangeSwitch = 2;
+					String temp1 = rangBytes.substring(0, rangBytes.indexOf("-"));
+					String temp2 = rangBytes.substring(rangBytes.indexOf("-") + 1, rangBytes.length());
+					p = Long.parseLong(temp1);
+					toLength = Long.parseLong(temp2);
+					contentLength = toLength - p + 1; // 客户端请求的是 270000-320000 之间的字节
+				}
+			} else {
+				contentLength = fileLength;
 			}
+			// 如果设设置了Content-Length，则客户端会自动进行多线程下载。如果不希望支持多线程，则不要设置这个参数。
+			// Content-Length: [文件的总大小] - [客户端请求的下载的文件块的开始字节]
+			response.setHeader("Content-Length", new Long(contentLength).toString());
+
+			// 断点开始
+			// 响应的格式是:
+			// Content-Range: bytes [文件块的开始字节]-[文件的总大小 - 1]/[文件的总大小]
+			if (rangeSwitch == 1) {
+				String contentRange = new StringBuffer("bytes ").append(new Long(p).toString()).append("-")
+						.append(new Long(fileLength - 1).toString()).append("/")
+						.append(new Long(fileLength).toString()).toString();
+				response.setHeader("Content-Range", contentRange);
+				bis.skip(p);
+			} else if (rangeSwitch == 2) {
+				String contentRange = range.replace("=", " ") + "/" + new Long(fileLength).toString();
+				response.setHeader("Content-Range", contentRange);
+				bis.skip(p);
+			} else {
+				String contentRange = new StringBuffer("bytes ").append("0-").append(fileLength - 1).append("/")
+						.append(fileLength).toString();
+				response.setHeader("Content-Range", contentRange);
+			}
+
+
+			inputStream = new BufferedInputStream(new FileInputStream(filePath));
+			OutputStream out = response.getOutputStream();
+			int n = 0;
+			long readLength = 0;
+			int bsize = 1024;
+			byte[] bytes = new byte[bsize];
+			if (rangeSwitch == 2) {
+				// 针对 bytes=27000-39000 的请求，从27000开始写数据
+				while (readLength <= contentLength - bsize) {
+					n = bis.read(bytes);
+					readLength += n;
+					out.write(bytes, 0, n);
+				}
+				if (readLength <= contentLength) {
+					n = bis.read(bytes, 0, (int) (contentLength - readLength));
+					out.write(bytes, 0, n);
+				}
+			} else {
+				while ((n = bis.read(bytes)) != -1) {
+					out.write(bytes, 0, n);
+				}
+			}
+			out.flush();
+			out.close();
+			bis.close();
 			response.flushBuffer();
 		} catch (IOException e) {
 			log.error("预览文件失败" + e.getMessage());
